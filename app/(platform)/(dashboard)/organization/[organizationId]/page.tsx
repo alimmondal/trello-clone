cbd6a7fd3b34ca8d7f61e91e21ebcf5a5353acc3
alimmondal <italimbd@gmail.com>
2024-01-21 18:44:19 +0600

    upd

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,5 @@
+const OrganizationPage = () => {
+  return <div className="h-full">OrganizationPage</div>;
+};
+
+export default OrganizationPage;
---
+++ config/site.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+  name: "Task-Hacks",
+  description:
+    "Manage projects, Collaborate, and reach new productivity peaks.",
+};
